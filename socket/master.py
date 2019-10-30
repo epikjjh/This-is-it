fcf5f7f5f7dd4247452a831aeb60bf455e190d0e
epikjjh <epikjjh@gmail.com>
2019-10-30 10:27:06 +0000

    Improve socket connection code by using thread
    Signed-off-by: epikjjh <epikjjh@gmail.com>

--- socket/master.py
+++ socket/master.py
@@ -1,8 +1,29 @@
 from socket import *
+import threading
+import time
+
+def send(sock):
+    while True:
+        data = input()
+        sock.send(data.encode("utf-8"))
+    sock.close()
+    
+def receive(sock):
+    while True:
+        data = sock.recv(1024).decode("utf-8")
+        print(data)
+    sock.close()
 
 master = socket(AF_INET, SOCK_STREAM)
 master.bind(("127.0.0.1", 80))
 master.listen(1)
 con, addr = master.accept()
-con.send("Hi".encode("utf-8"))
-print(con.recv(1024).decode("utf-8"))
+slave = socket(AF_INET, SOCK_STREAM)
+slave.connect(("127.0.0.1", 80))
+sender = threading.Thread(target=send, args=(con,))
+receiver = threading.Thread(target=receive, args=(con,))
+sender.start()
+receiver.start()
+while True:
+    time.sleep(1)
+    pass
--- socket/slave.py
+++ socket/slave.py
@@ -1,6 +1,24 @@
 from socket import *
+import threading
+import time
 
+def send(sock):
+    while True:
+        data = input()
+        sock.send(data.encode("utf-8"))
+    sock.close()
+    
+def receive(sock):
+    while True:
+        data = sock.recv(1024).decode("utf-8")
+        print(data)
+    sock.close()
 slave = socket(AF_INET, SOCK_STREAM)
 slave.connect(("127.0.0.1", 80))
-print(slave.recv(1024).decode("utf-8"))
-slave.send("Bye".encode("utf-8"))
+sender = threading.Thread(target=send, args=(slave,))
+receiver = threading.Thread(target=receive, args=(slave,))
+sender.start()
+receiver.start()
+while True:
+    time.sleep(1)
+    pass
